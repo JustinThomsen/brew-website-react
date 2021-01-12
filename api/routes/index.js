3650ed08d4641137c494ca3de3b05b3693324f24
justinthomsen <justinrobertthomsen@gmail.com>
2021-01-11 21:34:25 -0600

    add linting and reformat some things

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
 const AWS = require('aws-sdk');
-let directory = path.join(__dirname, '..', '..', 'app', 'build');
-let beverages = require('../public/data/beverages.json');
 
-let apikey = process.env.BREW_KEY;
+const directory = path.join(__dirname, '..', '..', 'app', 'build');
 const axios = require('axios');
-axios.defaults.headers.common['X-API-Key'] = apikey;
 
-let ankeny = {};
-let bettendorf = {};
+const apikey = process.env.BREW_KEY;
+const beverages = require('../public/data/beverages.json');
+
+axios.defaults.headers.common['X-API-Key'] = apikey;
 
 AWS.config.update({
   accessKeyId: process.env.AWS_IAM,
@@ -20,86 +20,86 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-router.use(express.static(directory));
-
-router.get('/', function (req, res) {
-  res.sendFile(path.join(directory, 'index.html'));
-});
-router.get('/ankeny', function (req, res) {
-  res.sendFile(path.join(directory, 'index.html'));
-});
-router.get('/bettendorf', function (req, res) {
-  res.sendFile(path.join(directory, 'index.html'));
-});
-router.get('/menu/', function (req, res) {
-  res.sendFile(path.join(directory, 'index.html'));
-});
-router.get('/api/bettendorf', function (req, res) {
-  this.bettendorf = retrieveFile('bettendorf.json')
-    .then(response => {
-      this.bettendorf = response;
-      res.send(this.bettendorf);
-    }, (error) => {
-      console.log(error);
-    });
-});
-router.get('/api/beverages', function (req, res) {
-  res.send(beverages);
-});
-router.get('/api/ankeny', function (req, res) {
-  this.ankeny = retrieveFile('ankeny.json')
-    .then(response => {
-      this.ankeny = response;
-      res.send(this.ankeny);
-    }, (error) => {
-      console.log(error);
-    });
-});
-
-router.get('/api/fermentationDetails/', function (req, res) {
-  let brewSessions = getBrewsessions();
-  brewSessions
-    .then((result) => {
-      let beerFermentationStatus = getFermentationDataByBeerName(result, req.query.name);
-      res.send(JSON.stringify(beerFermentationStatus));
-    });
-});
-
 async function retrieveFile(filename, res) {
-
   try {
     const getParams = {
       Bucket: 'elasticbeanstalk-us-west-2-381871787484',
-      Key: filename
+      Key: filename,
     };
 
-    let response = await s3.getObject(getParams)
+    const response = await s3.getObject(getParams)
       .promise();
-    let beerListAsJson = JSON.parse(response.Body);
-    return beerListAsJson;
-
+    return JSON.parse(response.Body);
   } catch (err) {
     return res.status(400)
       .send({
         success: false,
-        err: err
+        err,
       });
   }
 }
 
-async function getBrewsessions() {
-  let sessions = await axios.get('/v1/brewsessions', { baseURL: 'https://api.brewersfriend.com' })
-    .then(response => {
-      return response.data;
-    }, (error) => {
+async function getBrewSessions() {
+  const sessions = await axios.get('/v1/brewsessions', { baseURL: 'https://api.brewersfriend.com' })
+    .then((response) => response.data, (error) => {
       console.log(error);
     });
-  return sessions.brewsessions;
+  return sessions.brewessions;
 }
 
 function getFermentationDataByBeerName(brewSessions, beerName) {
-  let beerData = brewSessions.filter(session => session.recipe_title === beerName);
-  return beerData;
+  return brewSessions.filter((session) => session.recipe_title === beerName);
 }
+
+router.use(express.static(directory));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(directory, 'index.html'));
+});
+
+router.get('/ankeny', (req, res) => {
+  res.sendFile(path.join(directory, 'index.html'));
+});
+
+router.get('/bettendorf', (req, res) => {
+  res.sendFile(path.join(directory, 'index.html'));
+});
+
+router.get('/menu/', (req, res) => {
+  res.sendFile(path.join(directory, 'index.html'));
+});
+
+router.get('/api/bettendorf', (req, res) => {
+  let bettendorf = retrieveFile('bettendorf.json')
+    .then((response) => {
+      bettendorf = response;
+      res.send(bettendorf);
+    }, (error) => {
+      console.log(error);
+    });
+});
+
+router.get('/api/beverages', (req, res) => {
+  res.send(beverages);
+});
+
+router.get('/api/ankeny', (req, res) => {
+  let ankeny = retrieveFile('ankeny.json')
+    .then((response) => {
+      ankeny = response;
+      res.send(ankeny);
+    }, (error) => {
+      console.log(error);
+    });
+});
+
+router.get('/api/fermentationDetails/', (req, res) => {
+  const brewSessions = getBrewSessions();
+  brewSessions
+    .then((result) => {
+      const beerFermentationStatus = getFermentationDataByBeerName(result, req.query.name);
+      res.send(JSON.stringify(beerFermentationStatus));
+    });
+});
 
 module.exports = router;
